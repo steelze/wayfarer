c6049662fc582b004c7f9ad38763927bbd4afd81
Steelze <sogungbure@gmail.com>
2019-07-01 10:30:01 +0100

    [chore #166950071] Fix ESlint errors on test suite

--- test/example.js
+++ test/example.js
@@ -7,40 +7,36 @@ chai.use(chaiHttp);
 
 
 describe('Test Mocha And Chai', () => {
-    describe('sayHello', () => {
-        it('should say Hello World!', () => {
-            const str = 'Hello World!';
-            expect(str).to.equal('Hello World!');
-        });
+  describe('sayHello', () => {
+    it('should say Hello World!', () => {
+      const str = 'Hello World!';
+      expect(str).to.equal('Hello World!');
     });
-    
-    describe('sayHello', () => {
-        it('should not say Hello World!', () => {
-            const str = 'Hello!';
-            expect(str).to.not.equal('Hello World!');
-        });
-    });
+  });
 
-    describe('Other Sample Test', () => {
-        it('should have property name with value Figo', () => {
-            const car = {name:'Figo', Maker:'Ford'};
-            car.should.have.property('name').equal('Figo');
-        });
-        it('Checking for null', () => {
-            const car = null;
-            should.not.exist(car);
-        });
+  describe('sayHello', () => {
+    it('should not say Hello World!', () => {
+      const str = 'Hello!';
+      expect(str).to.not.equal('Hello World!');
     });
+  });
 
-    describe('Tets Request', () => {
-        it('Home page', (done) => {
-            chai.request(app)
-            .get('/')
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                expect(res.body).to.have.property('msg', 'Hello World');
-                done();
-            });
+  describe('Other Sample Test', () => {
+    it('should have property name with value Figo', () => {
+      const car = { name: 'Figo', Maker: 'Ford' };
+      car.should.have.property('name').equal('Figo');
+    });
+  });
+
+  describe('Tets Request', () => {
+    it('Home page', (done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.have.property('msg', 'Hello World');
+          done();
         });
     });
+  });
 });
