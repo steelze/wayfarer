0a9af57d11b9d0eecb9b4a0051501934b106fdf9
Steelze <sogungbure@gmail.com>
2019-07-05 14:29:19 +0100

    [feature #167103802] Return list of trips on the system
    - Write tests for this functionality
    
    [Finishes #167103802]

--- src/middlewares/AuthMiddleware.js
+++ src/middlewares/AuthMiddleware.js
@@ -5,14 +5,13 @@ import ErrorHandler from '../util/ErrorHandler';
 
 const { secret: SECRET_KEY } = Config;
 
-async function verifyToken(token) {
-  return jwt.verify(token, SECRET_KEY);
-}
-
 export default function (req, res, next) {
   const token = req.headers.authorization;
   if (!token) return next(ErrorHandler.error('Unauthenticated user', 401));
-  return verifyToken(token)
-    .then(() => next())
-    .catch(error => next(ErrorHandler.error(error.message, 401)));
+  try {
+    jwt.verify(token, SECRET_KEY);
+    return next();
+  } catch (error) {
+    return next(ErrorHandler.error(error.message, 401));
+  }
 }
