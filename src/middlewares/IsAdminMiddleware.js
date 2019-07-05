c7272a9eec907cc4ad6085a9562f5a9a4d78abc7
Steelze <sogungbure@gmail.com>
2019-07-05 16:21:02 +0100

    [feature #167072858] Admin can create a trip

--- src/middlewares/IsAdminMiddleware.js
+++ src/middlewares/IsAdminMiddleware.js
@@ -8,12 +8,8 @@ const { secret: SECRET_KEY } = Config;
 
 export default function (req, res, next) {
   const token = req.headers.authorization;
-  try {
-    const decoded = jwt.verify(token, SECRET_KEY);
-    const { id } = decoded;
-    const isAdmin = User.isAdmin(id);
-    return isAdmin ? next() : next(ErrorHandler.error('Unauthorized', 403));
-  } catch (error) {
-    return next(ErrorHandler.error(error.message, 401));
-  }
+  const decoded = jwt.verify(token, SECRET_KEY);
+  const { id } = decoded;
+  const isAdmin = User.isAdmin(id);
+  return isAdmin ? next() : next(ErrorHandler.error('Unauthorized', 403));
 }
